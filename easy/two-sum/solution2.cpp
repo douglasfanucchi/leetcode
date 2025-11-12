bd9ecc5ec36434c35cd259343e6503ef40337212
Douglas Fanucchi <douglasfanucchi@gmail.com>
2025-11-12 19:15:04 -0300

    adds O(n) solution to two-sums problem

---
+++ easy/two-sum/solution2.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> twoNumberSum(vector<int> array, int targetSum) { // O(n)
+  std::unordered_map<int, bool> map;
+  for(int i = 0; i < array.size(); i++) {
+    map[array[i]] = true;
+  }
+
+  int diff;
+  for(int i = 0; i < array.size(); i++) {
+    diff = targetSum - array[i];
+    if (diff == array[i]) {
+      continue;
+    }
+    if (map.find(diff) != map.end()) {
+      return {array[i], diff};
+    }
+  }
+  return {};
+}
